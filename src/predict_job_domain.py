489246cef8d7fcb847218313d76f585882705f31
Prashasti Gupta <118652336+prashastigupta2004@users.noreply.github.com>
2024-05-15 16:42:51 +0530

    Add files via upload

--- src/predict_job_domain.py
+++ src/predict_job_domain.py
@@ -1,25 +1,21 @@
 import pickle
-def predict_job_domain_for_user(skills):
 
+def predict_job_domain_for_user(skills):
+    # Load the trained SVM classifier and TF-IDF vectorizer
     with open('svm_classifier_model.pkl', 'rb') as file:
         loaded_model = pickle.load(file)
-
-    # Load the saved vectorizer from the pickle file
     with open('vectorizer.pkl', 'rb') as file:
         loaded_vectorizer = pickle.load(file)
-    
 
-    
+    # Preprocess the user skills
+    preprocessed_skills = skills.lower()  # You can add more preprocessing steps here
 
-    # Preprocess the single comment in the same way as your training data
-    # For example, you may need to convert to lowercase, remove punctuation, etc.
+    # Transform preprocessed skills into TF-IDF features using the loaded vectorizer
+    skills_tfidf = loaded_vectorizer.transform([preprocessed_skills])
 
-    # Transform the preprocessed comment into TF-IDF features using the loaded vectorizer
-    X_single_comment_tfidf = loaded_vectorizer.transform([skills.replace(',',' ').lower()])
+    # Predict the job domain for the user skills
+    predicted_domain = loaded_model.predict(skills_tfidf)
 
-    # Predict sentiment for the single comment
-    predicted_sentiment = loaded_model.predict([X_single_comment_tfidf])
+    return predicted_domain[0]
 
-    print("Predicted sentiment for the single comment: ye src se ", predicted_sentiment)
-    return predicted_sentiment[0]
 
